5ca4e65e6b7a7a62ba4e278364f6ff1a35af7444
Stanis1av <krasavin.stanislaus@gmail.com>
2021-12-03 22:50:08 +0400

    Locale detection via path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,19 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale_from_subdomain
-    # detecting the current locale (request = http://localhost:3000/)
-    detected_locale = request.host.split('.').first
+    detected_locale = request.host.split('.').first # Set locale via subdomen
+    detected_locale = request.fullpath.split('/').second # set locale via path
+    # logger.debug "detected_locale = #{detected_locale}"
 
     # Find and use an available locale. Otherwise, the default locale is used.
     I18n.locale = I18n.available_locales.map(&:to_s).include?(detected_locale) ? detected_locale : browser_locale
 
     # Debug logger
     # logger.debug "browser_locale: #{browser_locale}"
-    logger.debug "current host: #{request.host.inspect}"
-    logger.debug "first host element: #{request.host.split('.').first.inspect}"
-    logger.debug "available locales = #{I18n.available_locales.map(&:to_s).inspect}"
-    logger.debug "I18n.locale = #{I18n.locale}"
+    # logger.debug "current host: #{request.host.inspect}"
+    # logger.debug "first host element: #{request.host.split('.').first.inspect}"
+    # logger.debug "available locales = #{I18n.available_locales.map(&:to_s).inspect}"
+    # logger.debug "I18n.locale = #{I18n.locale}"
   end
 
   def browser_locale
