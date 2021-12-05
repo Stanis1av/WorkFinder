9e745987a151530e761a2f92ece91fdec7f72ea2
Stanis1av <krasavin.stanislaus@gmail.com>
2021-12-05 23:58:03 +0400

    Generating migrations Company and JobSeeker

--- db/migrate/20211205194214_create_job_seekers.rb
+++ db/migrate/20211205194214_create_job_seekers.rb
@@ -1,6 +1,11 @@
 class CreateJobSeekers < ActiveRecord::Migration[6.1]
   def change
     create_table :job_seekers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_26_184520) do
+ActiveRecord::Schema.define(version: 2021_12_05_194227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.text "about_company"
+    t.string "website"
+    t.string "email"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "job_seekers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_job_seekers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,9 +49,12 @@ ActiveRecord::Schema.define(version: 2021_11_26_184520) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "companies", "users"
+  add_foreign_key "job_seekers", "users"
 end
