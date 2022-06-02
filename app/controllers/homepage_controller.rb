fc935bfbea86084b4a884b9cfcde46fceac07911
Stanis1av <krasavin.stanislaus@gmail.com>
2022-06-02 23:26:04 +0400

    test 2

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,22 +1,22 @@
 class HomepageController < ApplicationController
   def index
-    @resumes = resumes
-    @query = query
+  #   @resumes = resumes
+  #   @query = query
   end
 
-  private
+  # private
 
-  def resumes
-    if query
-      Resume.where("headline ILIKE ?", "%#{query}%")
+  # def resumes
+  #   if query
+  #     Resume.where("headline ILIKE ?", "%#{query}%")
 
-      # Resume.joins(:skills).where("name ILIKE ?", "%#{query}%")
-    else
-      Resume.all
-    end
-  end
+  #     # Resume.joins(:skills).where("name ILIKE ?", "%#{query}%")
+  #   else
+  #     Resume.all
+  #   end
+  # end
 
-  def query
-    params[:query]
-  end
+  # def query
+  #   params[:query]
+  # end
 end
