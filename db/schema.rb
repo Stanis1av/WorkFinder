e923e7815d457254dda477fc1058ff9588fdadd8
Stanis1av <krasavin.stanislaus@gmail.com>
2022-06-10 12:00:33 +0400

    Minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -168,13 +168,13 @@ ActiveRecord::Schema.define(version: 2022_03_27_230616) do
 
   create_table "work_experiences", force: :cascade do |t|
     t.string "job_title"
+    t.text "description"
     t.string "company"
     t.string "country_of_work"
     t.string "city_or_state_of_work"
     t.boolean "i_currently_work_here"
     t.datetime "from"
     t.datetime "to"
-    t.text "description"
     t.bigint "resume_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
